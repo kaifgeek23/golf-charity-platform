77be8a1d5865a514d8624b4eaadc07e5891b7acb
kaifgeek23 <kaif.231850.ci@mhssce.ac.in>
2026-03-26 13:22:10 +0530

    connected backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://your-backend-url.onrender.com"
+  baseURL: "https://golf-charity-platform-esvx.onrender.com"
 });
 
 export default api;
